acd70ad98604d242314661925424eef200124a71
Altheo Saquilayan <altheo.saquilayan@exist.com>
2025-11-20 23:35:47 +0800

    PHSHOE-60: Refactor commons to only have commons code not specific microservice code

--- ph-shoes-services-common-core/src/main/java/com/nimbly/phshoesbackend/services/common/core/model/VerificationStatus.java
+++
@@ -1,5 +0,0 @@
-package com.nimbly.phshoesbackend.services.common.core.model;
-
-public enum VerificationStatus {
-    PENDING, VERIFIED, EXPIRED, FAILED, UNKNOWN
-}
--- ph-shoes-services-common-core/src/main/java/com/nimbly/phshoesbackend/services/common/core/repository/AccountSettingsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nimbly.phshoesbackend.services.common.core.repository;
-
-import java.util.Optional;
-
-public interface AccountSettingsRepository {
-    Optional<String> getSettingsJson(String userId);
-
-    void putSettingsJson(String userId, String settingsJson);
-}
